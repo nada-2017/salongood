665c56611c22b5c9f32d2a381388568d6c341536
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-05-31 01:39:20 +0300

    Update DTO.java

--- Salon1/src/main/java/com/example/salon1/DTO/DTO.java
+++ Salon1/src/main/java/com/example/salon1/DTO/DTO.java
@@ -1,4 +1,16 @@
 package com.example.salon1.DTO;
 
+@Data
+@AllArgsConstructor
 public class DTO {
+  
+    private Integer customer_id;
+    
+    private Integer day;
+    
+    private Integer month;
+    
+    private Integer year;
+    
+    private Integer hour;
 }
